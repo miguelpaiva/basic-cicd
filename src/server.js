9a17e5fa1521f35739ace943bc923d3666678d25
miguelpaiva <miguelpaiva91@gmail.com>
2021-12-20 22:12:11 -0300

    primeiro commit

---
+++ src/server.js
@@ -0,0 +1,11 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const server = (0, express_1.default)();
+server.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+server.listen(3333);
